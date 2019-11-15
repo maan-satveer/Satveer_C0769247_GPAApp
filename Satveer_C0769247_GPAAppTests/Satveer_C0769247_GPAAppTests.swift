840ebc8858661ac4462c6d4e453a46b07665ed2d
maan-satveer <koursatveer12@gmail.com>
2019-11-15 13:55:27 -0500

    Initial Commit

---
+++ Satveer_C0769247_GPAAppTests/Satveer_C0769247_GPAAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Satveer_C0769247_GPAAppTests.swift
+//  Satveer_C0769247_GPAAppTests
+//
+//  Created by MacStudent on 2019-11-15.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import XCTest
+@testable import Satveer_C0769247_GPAApp
+
+class Satveer_C0769247_GPAAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
